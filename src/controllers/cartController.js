b080a69016fa6a8dd37c508d12416ade887deb46
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-21 01:52:19 +0600

    added api to update cart

--- src/controllers/cartController.js
+++ src/controllers/cartController.js
@@ -1,3 +1,4 @@
+const Cart = require("../models/Cart");
 const cartService = require("../services/cartService");
 const addToCart = async (req, res) => {
   try {
@@ -7,6 +8,34 @@ const addToCart = async (req, res) => {
     res
       .status(200)
       .send({ success: true, data: cart, message: "Product added to cart" });
+  } catch (error) {
+    res.status(400).send({ success: false, message: error.message });
+  }
+};
+
+const updateCart = async (req, res) => {
+  try {
+    const { id } = req.user;
+    const { cartItems } = req.body;
+    const cart = await cartService.updateCart(id, cartItems);
+    return res.send({
+      success: true, data: cart, message: "Updated cart"
+    });
+  } catch (error) {
+    res.status(400).send({ success: false, message: error.message });
+  }
+};
+
+const updateQuantity = async (req, res) => {
+  try {
+    const { user } = req;
+    const { id } = req.params;
+    const { quantity } = req.body;
+    const cart = await cartService.updateQuantity(user?.id, id, quantity);
+    console.log(cart);
+    res
+      .status(200)
+      .send({ success: true, data: cart, message: "Quantity updated" });
   } catch (error) {
     res.status(400).send({ success: false, message: error.message });
   }
@@ -27,10 +56,20 @@ const removeCartItem = async (req, res) => {
     const { user } = req;
     const { productId } = req.params;
     const cart = await cartService.removeCartItem(user, productId);
-    res.status(200).send({ success: true, data: cart });
+    res.status(200).send({
+      success: true,
+      data: cart,
+      message: "The item is removed successfully",
+    });
   } catch (error) {
     res.status(400).send({ success: false, message: error.message });
   }
 };
 
-module.exports = { addToCart, getCartItems, removeCartItem };
+module.exports = {
+  addToCart,
+  getCartItems,
+  removeCartItem,
+  updateQuantity,
+  updateCart,
+};
--- src/services/cartService.js
+++ src/services/cartService.js
@@ -5,18 +5,77 @@ const addToCart = async (userId, productId, quantity) => {
   try {
     const product = await Product.findById(productId);
     if (!product) throw new Error("Product not found");
-    const cart = await Cart.findOneAndUpdate(
-      { user: userId },
-      {
-        $push: {
-          products: {
-            product: productId,
-            quantity,
-          },
-        },
-      },
-      { new: true, upsert: true }
+
+    let cart = await Cart.findOneAndUpdate(
+      { user: userId, "products.product": productId },
+      { $inc: { "products.$.quantity": quantity } },
+      { new: true }
     );
+
+    if (!cart) {
+      cart = await Cart.findOneAndUpdate(
+        { user: userId },
+        { $push: { products: { product: productId, quantity } } },
+        { new: true, upsert: true }
+      );
+    }
+
+    return cart;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const updateCart = async (userId, cartItems) => {
+  try {
+    const cart = await Cart.findOne({ user: userId });
+
+    if (!cart) {
+      return await Cart.create({
+        user: userId,
+        products: cartItems.map((item) => ({
+          product: item.product._id,
+          quantity: item.quantity,
+        })),
+      });
+    }
+
+    const existingProductIds = cart.products.map((item) =>
+      item.product.toString()
+    );
+
+    const newItems = cartItems.filter(
+      (item) => !existingProductIds.includes(item.product._id.toString())
+    );
+
+    if (newItems.length > 0) {
+      cart.products.push(
+        ...newItems.map((item) => ({
+          product: item.product._id,
+          quantity: item.quantity,
+        }))
+      );
+      await cart.save();
+    }
+    const updatedCart = await Cart.findOne({ user: userId }).populate(
+      "products.product"
+    );
+    console.log(userId);
+    return updatedCart;
+  } catch (error) {
+    throw new Error(error.message);
+  }
+};
+
+const updateQuantity = async (userId, productId, quantity) => {
+  try {
+    const cart = await Cart.findOneAndUpdate(
+      { user: userId, "products.product": productId },
+      {
+        $inc: { "products.$.quantity": quantity },
+      },
+      { new: true }
+    ).populate("products.product");
     return cart;
   } catch (error) {
     throw new Error(error.message);
@@ -40,11 +99,17 @@ const removeCartItem = async (user, productId) => {
       { user: user.id },
       { $pull: { products: { product: productId } } },
       { new: true }
-    );
+    ).populate("products.product");
     return cart;
   } catch (error) {
     throw new Error(error.message);
   }
 };
 
-module.exports = { addToCart, getCartItems, removeCartItem };
+module.exports = {
+  addToCart,
+  getCartItems,
+  removeCartItem,
+  updateQuantity,
+  updateCart,
+};
