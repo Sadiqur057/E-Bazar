db46d5587cf273da650e0510f354f56cee03d6f9
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-21 01:53:12 +0600

    added routes for update cart

--- src/routes/cartRoutes.js
+++ src/routes/cartRoutes.js
@@ -1,11 +1,19 @@
-const express = require('express');
-const { addToCart, getCartItems, removeCartItem } = require('../controllers/cartController.js');
-const { verifyToken } = require('../middlewares/auth.js');
+const express = require("express");
+const {
+  addToCart,
+  getCartItems,
+  removeCartItem,
+  updateQuantity,
+  updateCart,
+} = require("../controllers/cartController.js");
+const { verifyToken } = require("../middlewares/auth.js");
 
 const router = express.Router();
 
-router.post('/add', verifyToken, addToCart);
-router.get('/', verifyToken, getCartItems);
-router.delete('/remove/:productId', verifyToken, removeCartItem);
+router.post("/add", verifyToken, addToCart);
+router.get("/", verifyToken, getCartItems);
+router.patch("/update-quantity/:id", verifyToken, updateQuantity);
+router.patch("/update-cart", verifyToken, updateCart);
+router.delete("/remove/:productId", verifyToken, removeCartItem);
 
 module.exports = router;
