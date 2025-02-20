fe804b639e8851363c1b79f32c31d0ac18a454ce
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-21 01:52:03 +0600

    added cart item sending to client after login

--- src/controllers/authController.js
+++ src/controllers/authController.js
@@ -1,15 +1,14 @@
 const authService = require("../services/authServices");
+const { getCartItems } = require("../services/cartService");
 
 const registerUser = async (req, res) => {
   try {
     const user = await authService.register(req.body);
-    res
-      .status(201)
-      .send({
-        message: "User registered successfully",
-        success: true,
-        data: { name: user?.name, email: user?.email, _id: user?._id },
-      });
+    res.status(201).send({
+      message: "User registered successfully",
+      success: true,
+      data: { name: user?.name, email: user?.email, _id: user?._id },
+    });
   } catch (error) {
     return res.status(409).send({ message: error.message, success: false });
   }
@@ -18,10 +17,17 @@ const registerUser = async (req, res) => {
 const loginUser = async (req, res) => {
   try {
     const { token, user } = await authService.login(req.body);
+    const cart = await getCartItems(user);
     res.send({
       message: "Login successful",
       token,
-      data: { name: user?.name, email: user?.email, _id: user?._id },
+      data: {
+        name: user?.name,
+        email: user?.email,
+        _id: user?._id,
+        role: user?.role,
+      },
+      cart: cart,
       success: true,
     });
   } catch (error) {
