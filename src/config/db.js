f244d2942283a46c1fa0b7efa613783cccaef50d
Md. Sadiqur Rahman <sadiqur057@gmail.com>
2025-02-21 21:31:55 +0600

    updated mongoose connection

--- src/config/db.js
+++ src/config/db.js
@@ -1,12 +1,23 @@
 const mongoose = require("mongoose");
 
+let isConnected = false; 
 const connectDB = async () => {
+  if (isConnected) {
+    console.log("Using existing MongoDB connection");
+    return;
+  }
+
   try {
-    await mongoose.connect(process.env.MONGO_URI);
+    const db = await mongoose.connect(process.env.MONGO_URI, {
+
+      serverSelectionTimeoutMS: 5000,
+    });
+
+    isConnected = db.connections[0].readyState; 
     console.log("MongoDB Connected");
   } catch (error) {
-    console.error(`Error: ${error.message}`);
-    process.exit(1);
+    console.error("MongoDB Connection Error:", error);
+    throw error;
   }
 };
 
